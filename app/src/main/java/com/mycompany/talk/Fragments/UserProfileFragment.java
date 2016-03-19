3c1c368b8b831d5f4f09d1e9c8aa820616ea4baf
Joshua Cantero <cante008@cougars.csusm.edu>
2016-03-18 21:49:27 -0700

    Start profile fragment

--- app/src/main/java/com/mycompany/talk/Fragments/UserProfileFragment.java
+++ app/src/main/java/com/mycompany/talk/Fragments/UserProfileFragment.java
@@ -7,21 +7,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.mycompany.talk.Login;
 import com.mycompany.talk.R;
+import com.mycompany.talk.Talker;
+
+import java.util.ArrayList;
 
 /**
  * Created by joshua on 3/16/2016.
  */
 public class UserProfileFragment extends Fragment {
 
-    Button logout;
+    private TextView UserName;
+
+    private Button logout;
+    private ArrayList<Talker> talkers;
+    private Talker t;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_user_profile,container,false);
+
+        UserName = (TextView)v.findViewById(R.id.tvUserName);
+
+
 
         logout = (Button)v.findViewById(R.id.bLogout);
         logout.setOnClickListener(new View.OnClickListener() {
